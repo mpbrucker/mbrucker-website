1f8ac2d3d75d3e311df53839dbc041a3425ac50e
mpbrucker <mpbrucker@gmail.com>
2016-08-03 23:46:14 -0500

    Updated main page content

--- home/content/js/init.js
+++ home/content/js/init.js
@@ -12,7 +12,11 @@
 
     setTimeout(function() {
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	   $('div.wrapper-left').fitText(.3, {minFontSize: '50px', maxFontSize: '400px' });
+	   $('div.wrapper-right').fitText(.3, {minFontSize: '50px', maxFontSize: '400px' });
+
 	 }, 100);
+	
 
 
 /*----------------------------------------------------*/
@@ -83,17 +87,14 @@
 		var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
+	   if ( (y > h*.30)) {
+	      nav.addClass('opaque');
+		  nav.removeClass('transparent');
 	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
-      }
+	   else {
+		  nav.removeClass('opaque');
+		  nav.addClass('transparent');
+	   }
 
 	});
 
